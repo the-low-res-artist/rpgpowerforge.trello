e5997366ef322e0cb07ab5c04f9729a695e1f306
the-low-res-artist <pixelart.asset@gmail.com>
2025-11-23 14:07:45 +0100

    venv

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
+# create a venv
+python3 -m venv venv
+source venv/bin/activate
+
 # install dependencies
 pip install -r requirements.txt --upgrade
 
-# run continously
+# run once
 python3 main.py
+
+deactivate
